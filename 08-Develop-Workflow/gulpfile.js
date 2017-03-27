92375f9b64b3bc5362bf2137e582113c2f44e68e
Matt Payne <Payne@MattPayne.org>
2017-03-27 12:24:51 -0500

    broken boo

--- 08-Develop-Workflow/gulpfile.js
+++ 08-Develop-Workflow/gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
+var minify = require("gulp-babel-minify");
 
 gulp.task("default", function () {
   return gulp.src("src/**/*.js")
     .pipe(sourcemaps.init())
     .pipe(babel())
+   // .pipe(minify())
     .pipe(concat("bundle.js"))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("build"));
